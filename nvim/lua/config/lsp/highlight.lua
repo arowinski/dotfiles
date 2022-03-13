e4812ddaaa100c835847a40a42e4f9f819009c75
arowinski <arowinski02@gmail.com>
2022-03-13 19:01:23 +0100

    Vim - use solargraph for document highlights

--- nvim/lua/config/lsp/highlight.lua
+++ nvim/lua/config/lsp/highlight.lua
@@ -1,11 +1,6 @@
 local M = {}
 
 function M.setup(client)
-  -- does not work properly
-  if client.name == "solargraph" then
-    return
-  end
-
   if client.resolved_capabilities.document_highlight then
     require("illuminate").on_attach(client)
   end
