d45086d67714b7aed8cfabdc83d5d0038856ae0d
arowinski <arowinski02@gmail.com>
2022-07-26 21:49:47 +0200

    Vim - add string.replace util function

--- nvim/lua/util/string.lua
+++ nvim/lua/util/string.lua
@@ -6,4 +6,13 @@ function M.camelcase(str)
   return str.gsub(str, "_", "")
 end
 
+function M.replace(str, what, with)
+  -- escape pattern
+  what = string.gsub(what, "[%(%)%.%+%-%*%?%[%]%^%$%%]", "%%%1")
+  -- escape replacement
+  with = string.gsub(with, "[%%]", "%%%%")
+
+  return string.gsub(str, what, with)
+end
+
 return M
