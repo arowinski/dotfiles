b489a86d2dfc943782e2c8d1ec1a7ca056a48c50
arowinski <arowinski02@gmail.com>
2022-07-30 20:21:58 +0200

    Vim - close all floating windows with <ESC>

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -122,6 +122,17 @@ util.nmap("[Q", ":cfirst<CR>")
 -- copy to system clipboard
 util.nmap("<Leader>y", '"+y')
 
+vim.keymap.set("n", "<Esc>", function()
+  for _, win in ipairs(vim.api.nvim_list_wins()) do
+    local config = vim.api.nvim_win_get_config(win)
+
+    if config.relative ~= "" then
+      vim.api.nvim_win_close(win, false)
+      print("Closing window", win)
+    end
+  end
+end)
+
 vim.cmd([[
   cnoremap <C-a> <Home>
   cnoremap <C-e> <END>
