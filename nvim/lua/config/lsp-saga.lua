977203d42dff118cb30202a66aecfffd783a040f
arowinski <arowinski02@gmail.com>
2022-09-21 19:28:07 +0200

    Vim - remove lsp-saga table mappings

--- nvim/lua/config/lsp-saga.lua
+++ nvim/lua/config/lsp-saga.lua
@@ -1,12 +1,15 @@
 require("lspsaga").init_lsp_saga({
   code_action_lightbulb = {
     enable = true,
+    enable_in_insert = true,
+    cache_code_action = true,
     sign = false,
+    update_time = 150,
     sign_priority = 20,
     virtual_text = true,
   },
   code_action_keys = {
-    quit = { "q", "<ESC>" },
+    quit = "<ESC>",
     exec = "<CR>",
   },
   finder_action_keys = {
@@ -14,7 +17,7 @@ require("lspsaga").init_lsp_saga({
     vsplit = "s",
     split = "i",
     tabe = "t",
-    quit = { "q", "<ESC>" },
+    quit = "<ESC>",
     scroll_down = "<C-f>",
     scroll_up = "<C-b>",
   },
