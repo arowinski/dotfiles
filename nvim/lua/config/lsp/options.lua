6aad6ba168cb8d9ff82157cccd2ed7eba985bf1f
arowinski <arowinski02@gmail.com>
2022-10-23 18:25:30 +0200

    Vim - change LSP capabilities object generation

--- nvim/lua/config/lsp/options.lua
+++ nvim/lua/config/lsp/options.lua
@@ -2,8 +2,7 @@ local on_attach = function(_, _)
   require("config.lsp.mappings").setup()
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 return {
   on_attach = on_attach,
