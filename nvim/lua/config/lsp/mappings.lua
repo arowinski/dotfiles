8e1d8d6e0f4e0f8c7576e51ff6f1ca759e42b405
arowinski <arowinski02@gmail.com>
2022-11-08 18:21:40 +0100

    Vim - update LSP mappings

--- nvim/lua/config/lsp/mappings.lua
+++ nvim/lua/config/lsp/mappings.lua
@@ -2,25 +2,25 @@ local util = require("util")
 
 local M = {}
 
-local bmap = function(keys, action)
-  util.nmap(keys, action, { buffer = true })
+local bmap = function(mode, keys, action)
+  util.noremap(mode, keys, action, { buffer = true })
 end
 
 function M.setup()
-  bmap("gD", vim.lsp.buf.declaration)
-  bmap("gt", vim.lsp.buf.references)
-  bmap("<leader>p", vim.lsp.buf.format)
+  bmap("n", "gD", vim.lsp.buf.definition)
+  bmap("n", "gt", vim.lsp.buf.references)
+  bmap("n", "<leader>p", vim.lsp.buf.format)
 
-  bmap("<C-a>", "<CMD>Lspsaga code_action<CR>")
-  bmap("gh", "<CMD>Lspsaga lsp_finder<CR>")
-  bmap("gd", "<CMD>Lspsaga peek_definition<CR>")
-  bmap("gs", "<CMD>Lspsaga signature_help<CR>")
-  bmap("K", "<CMD>Lspsaga hover_doc<CR>")
-  util.inoremap("<C-s>", "<CMD>Lspsaga signature_help<CR>")
+  bmap({ "n", "v" }, "<C-a>", "<CMD>Lspsaga code_action<CR>")
+  bmap("n", "gh", "<CMD>Lspsaga lsp_finder<CR>")
+  bmap("n", "gd", "<CMD>Lspsaga peek_definition<CR>")
+  bmap("n", "gs", "<CMD>Lspsaga signature_help<CR>")
+  bmap("n", "K", "<CMD>Lspsaga hover_doc<CR>")
+  bmap("i", "<C-s>", "<CMD>Lspsaga signature_help<CR>")
 
-  bmap("[r", "<CMD>Lspsaga diagnostic_jump_prev<CR>")
-  bmap("]r", "<CMD>Lspsaga diagnostic_jump_next<CR>")
-  bmap("<leader>d", "<CMD>Lspsaga show_line_diagnostics<CR>")
+  bmap("n", "[r", "<CMD>Lspsaga diagnostic_jump_prev<CR>")
+  bmap("n", "]r", "<CMD>Lspsaga diagnostic_jump_next<CR>")
+  bmap("n", "<leader>d", "<CMD>Lspsaga show_line_diagnostics<CR>")
 end
 
 return M
