731a6ec1b8de3961a558008ce255c91f063b085f
arowinski <arowinski02@gmail.com>
2023-09-05 20:23:33 +0200

    Vim - update CMP configuration

--- nvim/lua/config/cmp.lua
+++ nvim/lua/config/cmp.lua
@@ -16,13 +16,15 @@ local cmp_autopairs = require("nvim-autopairs.completion.cmp")
 cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
 
 cmp.setup({
-  sources = {
+  sources = cmp.config.sources({
     { name = "luasnip" },
+    { name = "copilot" },
+    { name = "nvim_lua" },
     { name = "nvim_lsp" },
     { name = "path" },
-    { name = "nvim_lua" },
     { name = "buffer", keyword_length = 3 },
-  },
+    { name = "treesitter", priority = 7 },
+  }),
   mapping = {
     ["<C-d>"] = cmp.mapping(function(fallback)
       if cmp.visible({ select = false }) then
@@ -68,7 +70,17 @@ cmp.setup({
       cmp.config.compare.offset,
       cmp.config.compare.exact,
       cmp.config.compare.score,
-      cmp.config.compare.recently_user,
+      function(entry1, entry2)
+        local _, entry1_under = entry1.completion_item.label:find("^_+")
+        local _, entry2_under = entry2.completion_item.label:find("^_+")
+        entry1_under = entry1_under or 0
+        entry2_under = entry2_under or 0
+        if entry1_under > entry2_under then
+          return false
+        elseif entry1_under < entry2_under then
+          return true
+        end
+      end,
       cmp.config.compare.kind,
       cmp.config.compare.sort_text,
       cmp.config.compare.length,
@@ -83,5 +95,9 @@ cmp.setup({
     expand = function(args)
       require("luasnip").lsp_expand(args.body)
     end,
+  },
+  window = {
+    completion = cmp.config.window.bordered(),
+    documentation = cmp.config.window.bordered(),
   },
 })
