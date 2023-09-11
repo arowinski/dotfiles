726408b58269a1b1fdc063f6196fb60358aa410f
arowinski <arowinski02@gmail.com>
2023-09-11 18:37:27 +0200

    Vim - fix copilot completion

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -22,12 +22,27 @@ return {
       {
         "zbirenbaum/copilot-cmp",
         dependencies = {
-          "zbirenbaum/copilot.lua",
-          opts = {
-            suggestion = { enabled = false },
-            panel = { enabled = false },
+          {
+            "zbirenbaum/copilot.lua",
+            cmd = "Copilot",
+            build = ":Copilot auth",
+            opts = {
+              suggestion = { enabled = false },
+              panel = { enabled = false },
+            },
           },
         },
+        config = function(_, opts)
+          local copilot_cmp = require("copilot_cmp")
+          copilot_cmp.setup(opts)
+          -- attach cmp source whenever copilot attaches
+          -- fixes lazy-loading issues with the copilot cmp source
+          require("util.lsp").on_attach(function(client)
+            if client.name == "copilot" then
+              copilot_cmp._on_insert_enter({})
+            end
+          end)
+        end,
       },
     },
     opts = function()
--- nvim/lua/util/lsp.lua
+++ nvim/lua/util/lsp.lua
@@ -23,4 +23,14 @@ function M.is_attached()
   return #size > 0
 end
 
+function M.on_attach(on_attach)
+  vim.api.nvim_create_autocmd("LspAttach", {
+    callback = function(args)
+      local buffer = args.buf
+      local client = vim.lsp.get_client_by_id(args.data.client_id)
+      on_attach(client, buffer)
+    end,
+  })
+end
+
 return M
