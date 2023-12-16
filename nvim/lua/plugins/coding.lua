bad49a80145e92ff2c9b67936629b862e92f9ba1
arowinski <arowinski02@gmail.com>
2023-12-16 12:49:00 +0100

    Vim - format coding plugins file

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -66,8 +66,8 @@ return {
     "ggandor/leap.nvim",
     event = "VeryLazy",
     config = function()
-     vim.keymap.set({'n', 'x', 'o'}, 's', '<Plug>(leap-forward-to)')
-     vim.keymap.set({'n', 'x', 'o'}, 'S', '<Plug>(leap-backward-to)')
+      vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap-forward-to)")
+      vim.keymap.set({ "n", "x", "o" }, "S", "<Plug>(leap-backward-to)")
     end,
   },
   {
