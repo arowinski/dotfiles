11a3d67d30ab61dd2ae9d5095b01bab298600622
arowinski <arowinski02@gmail.com>
2024-01-13 08:12:59 +0100

    Vim - use built-in eslint LSP config

--- nvim/lua/plugins/lsp/config.lua
+++ nvim/lua/plugins/lsp/config.lua
@@ -3,7 +3,6 @@ local options = require("plugins.lsp.options")
 
 require("plugins.lsp.null-ls").setup(lsp, options)
 require("plugins.lsp.solargraph").setup(lsp, options)
-require("plugins.lsp.eslint").setup(lsp, options)
 require("plugins.lsp.lua-ls").setup(lsp, options)
 require("plugins.lsp.typescript").setup(lsp, options)
 
@@ -19,3 +18,4 @@ setup("tailwindcss", { root_dir = lsp.util.root_pattern("tailwind.config.js") })
 setup("html")
 setup("yamlls")
 setup("rubocop")
+setup("eslint")
