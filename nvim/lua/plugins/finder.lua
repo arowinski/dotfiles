1d730734994c3e99fd8caac4dc1f6fbb802ec4bb
arowinski <arowinski02@gmail.com>
2024-01-13 08:37:47 +0100

    Vim - add live-grep-args extension to telescope

--- nvim/lua/plugins/finder.lua
+++ nvim/lua/plugins/finder.lua
@@ -4,6 +4,10 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    {
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      version = "^1.0.0",
+    },
   },
   cmd = { "Telescope" },
   keys = function()
@@ -12,9 +16,15 @@ return {
     return {
       { "<leader>ff", builtin.find_files, desc = "Find files" },
       { "<leader>fb", builtin.buffers, desc = "List buffers" },
-      { "<leader>fe", builtin.live_grep, desc = "Live search" },
       { "<leader>fr", builtin.resume, desc = "Resume finder" },
       { "<leader>fc", builtin.command_history, desc = "List command history" },
+      {
+        "<leader>fe",
+        function()
+          require("telescope").extensions.live_grep_args.live_grep_args()
+        end,
+        desc = "Live search",
+      },
       {
         "<leader>fw",
         function()
@@ -37,6 +47,7 @@ return {
   end,
   opts = function()
     local actions = require("telescope.actions")
+    local lga_actions = require("telescope-live-grep-args.actions")
 
     return {
       defaults = {
@@ -67,11 +78,29 @@ return {
         },
       },
       extensions = {
-        fzy_native = {
-          override_generic_sorter = false,
+        fzf = {
+          fuzzy = true,
+          override_generic_sorter = true,
           override_file_sorter = true,
+          case_mode = "smart_case",
+        },
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = {
+            i = {
+              ["<C-m>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
         },
       },
     }
+  end,
+  config = function(_, options)
+    local telescope = require("telescope")
+
+    telescope.setup(options)
+    telescope.load_extension("fzf")
+    telescope.load_extension("live_grep_args")
   end,
 }
