37133f4f6ab48c1098b262d80dc07aef0cd9e723
arowinski <arowinski02@gmail.com>
2024-05-30 07:53:57 +0200

    Vim - replace deprecated LSP helper functions

--- nvim/lua/util/lsp.lua
+++ nvim/lua/util/lsp.lua
@@ -1,14 +1,14 @@
 local M = {}
 
 function M.is_ready()
-  local messages = vim.lsp.util.get_progress_messages()
+  local messages = vim.lsp.status()
 
   return #messages == 0
 end
 
 function M.is_attached()
   local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-  local clients = vim.lsp.get_active_clients({ bufnr = 0 })
+  local clients = vim.lsp.get_clients({ bufnr = 0 })
 
   if next(clients) == nil then
     return false
