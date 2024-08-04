4682bb08ef426a193e23b9edd16d5959151b28f7
arowinski <arowinski02@gmail.com>
2024-08-04 08:53:43 +0200

    Vim - stylua add config arguments

--- nvim/lua/plugins/formatting.lua
+++ nvim/lua/plugins/formatting.lua
@@ -1,27 +1,36 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		keys = {
-			{
-				"<leader>p",
-				function()
-					require("conform").format()
-				end,
-				"Format the current buffer",
-			},
-		},
-		opts = {
-			default_format_opts = {
-				quiet = false,
-				lsp_format = "fallback",
-			},
-			formatters_by_ft = {
-				lua = { "stylua" },
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				sh = { "shfmt" },
-				["*"] = { "typos" },
-				ruby = { "typos", lsp_format = "first" },
-			},
-		},
-	},
+  {
+    "stevearc/conform.nvim",
+    keys = {
+      {
+        "<leader>p",
+        function()
+          require("conform").format()
+        end,
+        "Format the current buffer",
+      },
+    },
+    opts = {
+      default_format_opts = {
+        quiet = false,
+        lsp_format = "fallback",
+      },
+      formatters_by_ft = {
+        lua = { "stylua" },
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        sh = { "shfmt" },
+        ["*"] = { "typos" },
+        ruby = { "typos", lsp_format = "first" },
+      },
+      formatters = {
+        stylua = {
+          append_args = {
+            "--indent-type=Spaces",
+            "--indent-width=2",
+            "--column-width=80",
+          },
+        },
+      },
+    },
+  },
 }
