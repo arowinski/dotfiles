b76ee5eedafd811b3bbf394b56c0e8eba300e8c5
arowinski <arowinski02@gmail.com>
2024-12-29 15:05:37 +0100

    Vim - improve the (R)spec snippet generation

--- nvim/lua/snip/ruby/util.lua
+++ nvim/lua/snip/ruby/util.lua
@@ -10,20 +10,30 @@ local function file_relative_path()
   )
 end
 
+local function app_dirs()
+  local app_dir = vim.fs.root(vim.fn.expand("%"), "Gemfile") .. "/app/"
+  local dirs = vim.fn.glob(app_dir .. "/*", false, 1)
+  local dir_names = {}
+
+  for _, dir in ipairs(dirs) do
+    table.insert(dir_names, vim.fn.fnamemodify(dir, ":t"))
+  end
+
+  return dir_names
+end
+
 function M.infer_const_parts()
   local parts = vim.split(file_relative_path(), "/")
-  local root = parts[1]
 
-  if root == "spec" then
+  if parts[1] == "spec" then
     parts = vim.tbl_map(function(e)
       return string.gsub(e, "_spec", "")
     end, parts)
   end
 
-  if root == "lib" or root == "spec" then
-    table.remove(parts, 1)
-  elseif root == "app" then
-    table.remove(parts, 1)
+  table.remove(parts, 1)
+
+  if vim.tbl_contains(app_dirs(), parts[1]) then
     table.remove(parts, 1)
   end
 
