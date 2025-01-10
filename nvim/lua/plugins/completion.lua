75cdcc7c22ce53920ac6eff893e9c2fe9814892f
arowinski <arowinski02@gmail.com>
2025-01-10 17:51:40 +0100

    Vim - update blink-cmp config

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -28,13 +28,10 @@ return {
       ["<C-b>"] = { "scroll_documentation_up", "fallback" },
       ["<C-f>"] = { "scroll_documentation_down", "fallback" },
       ["<C-q>"] = { "hide", "fallback" },
-      -- ["<F13>2"] = { "snippet_forward", "fallback" },
-      -- ["<F13>3"] = { "snippet_backward" },
+      ["<F13>2"] = { "snippet_forward" },
+      ["<F13>3"] = { "snippet_backward" },
     },
-
     appearance = {
-      use_nvim_cmp_as_default = true,
-      nerd_font_variant = "mono",
       kind_icons = {
         Text = "󰉿",
         Method = "󰆧",
@@ -65,7 +62,6 @@ return {
       },
     },
     completion = {
-      list = { selection = "auto_insert" },
       documentation = {
         auto_show = true,
         auto_show_delay_ms = 300,
@@ -84,37 +80,19 @@ return {
           },
         },
       },
-      ghost_text = {
-        enabled = true,
-      },
+      ghost_text = { enabled = true },
     },
-
-    snippets = {
-      expand = function(snippet)
-        require("luasnip").lsp_expand(snippet)
-      end,
-      active = function(filter)
-        if filter and filter.direction then
-          return require("luasnip").jumpable(filter.direction)
-        end
-        return require("luasnip").in_snippet()
-      end,
-      jump = function(direction)
-        require("luasnip").jump(direction)
-      end,
-    },
-
+    snippets = { preset = "luasnip" },
     sources = {
       default = { "lsp", "path", "snippets", "buffer", "copilot" },
       providers = {
         copilot = {
           name = "copilot",
           module = "blink-cmp-copilot",
-          score_offset = 5,
+          score_offset = 100,
           async = true,
           transform_items = function(_, items)
-            local CompletionItemKind =
-              require("blink.cmp.types").CompletionItemKind
+            local CompletionItemKind = require("blink.cmp.types").CompletionItemKind
             local kind_idx = #CompletionItemKind + 1
             CompletionItemKind[kind_idx] = "Copilot"
 
@@ -132,7 +110,6 @@ return {
       },
       cmdline = {}, -- disable cmdline completion
     },
-    -- fuzzy = { sorts = { "sort_text", "score" } },
   },
   opts_extend = { "sources.default" },
 }
