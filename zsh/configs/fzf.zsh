be762e11fa4348e79742f2b4b99fb939a8f54934
arowinski <arowinski02@gmail.com>
2025-01-24 19:07:10 +0100

    FZF - use C-l to accept

--- zsh/configs/fzf.zsh
+++ zsh/configs/fzf.zsh
@@ -3,7 +3,7 @@
 source <(fzf --zsh)
 
 export FZF_DEFAULT_COMMAND='rg --hidden --files -g ""'
-export FZF_DEFAULT_OPTS='--bind ctrl-space:abort --layout=reverse'
+export FZF_DEFAULT_OPTS='--bind ctrl-space:abort --layout=reverse --bind ctrl-l:accept'
 
 alias fzf="
   fzf \
