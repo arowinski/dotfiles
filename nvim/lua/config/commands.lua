3a8ea6b873fae78e5a3d9cfa1da6c69ed4974977
arowinski <arowinski02@gmail.com>
2025-11-29 15:57:06 +0100

    Vim - add :Remove command

--- nvim/lua/config/commands.lua
+++ nvim/lua/config/commands.lua
@@ -11,3 +11,14 @@ local function setup_command_alias(from, to)
 end
 
 setup_command_alias("E", "e")
+
+vim.api.nvim_create_user_command("Remove", function()
+  local path = vim.fn.expand("%:p")
+  if path == "" then
+    vim.notify("No file to remove", vim.log.levels.ERROR)
+    return
+  end
+  vim.fn.delete(path)
+  vim.cmd("bdelete!")
+  vim.notify("Removed " .. path)
+end, {})
